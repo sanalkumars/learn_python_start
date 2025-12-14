0fb2e561482a0c8df91e1864e6f410f2fa69c7f3
sanalkumars <sanalskumar126@gmail.com>
2025-12-14 10:50:21 +0530

    string funcation basics covered

---
+++ strings/string.py
@@ -0,0 +1,14 @@
+
+
+name = input("Enter ur name ")
+
+# lenght = len(name ) //finding the length
+# result = name.find("a")
+# result = name.capitalize()
+# result = name.upper() just like this we have lower to 
+# result = name.isdigit()
+# result = name.isalpha()  this nly return true is the string is alphabets only no space is allowed
+# result = name.count("s") gets the count of specific character
+# result = name.replace("s","S") replce the old string/first argument with the second one 
+
+print(f"result for the string operation is {result}")
